1ff18d6a18d632abe3d90e64b7257c6e5584c355
Ben Timby <btimby@gmail.com>
2022-08-01 11:44:20 -0400

    Use Buefy component.

--- web/src/components/Loading.vue
+++ web/src/components/Loading.vue
@@ -1,24 +1,19 @@
 <template>
-  <div
-    :class="{ overlay: true, hidden: !visible }"
-  >
-    <img
-      :src="spinner"
-      class="spinner"
-    />
-  </div>
+  <b-loading
+    v-model="visible"
+    :is-full-page="true"
+    :can-cancel="false"
+  ></b-loading>
 </template>
 
 <script>
 import axios from 'axios';
-import spinner from '@/assets/loading.gif';
 
 export default {
   name: 'Loading',
 
   data() {
     return {
-      spinner,
       count: 0,
     };
   },
@@ -61,37 +56,8 @@ export default {
         return Promise.reject(error);
       });
   },
-
-  unmounted() {
-
-  },
 }
 </script>
 
 <style scoped>
-.hidden {
-  display: none;
-}
-
-.overlay {
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  z-index: 1000;
-  left: 0;
-  top: 0;
-  background-color: rgb(0,0,0);
-  background-color: rgba(0,0,0, 0.9);
-  overflow: hidden;
-}
-
-.spinner {
-  width: 96px;
-  height: 96px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin-top: -48px; /* Half the height */
-  margin-left: -48px; /* Half the width */
-}
 </style>
